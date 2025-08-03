a0d75ffa0e073b9eb15bac5ce2b64e982ed59c56
Darshini Sai Mathi <159278720+DarshiniSai@users.noreply.github.com>
2025-08-03 11:49:23 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,3 @@
-const BACKEND_URL = "http://localhost:3000";
+const BACKEND_URL = window.location.hostname.includes("localhost")
+  ? "http://localhost:3000"
+  : "https://serene-dedication.up.railway.app";
